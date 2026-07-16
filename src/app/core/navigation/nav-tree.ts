1dcaf6e3cf45c480344b1cebb4dfdfca269c3bc4
Olivier Mignot <olivier.mignot83@outlook.fr>
2026-07-16 18:05:12 +0200

    modifications css et creation partielle du menu

--- src/app/core/navigation/nav-tree.ts
+++ src/app/core/navigation/nav-tree.ts
@@ -18,53 +18,404 @@ export interface NavTreeNode {
   children?: NavTreeNode[];
 }
 
-function buildBranch(rootLabel: string, routePrefix: string, exampleId: 1 | 2): NavTreeNode {
-  return {
-    name: rootLabel,
+export const NAV_TREE: NavTreeNode[] = [
+  {
+    name: 'Accueil',
+    route: `home`,
+  },
+  {    
+    name: 'Paramétrage',
     children: [
       {
-        name: `${rootLabel} spécifique 1`,
+        name: `Codification de base`,
         children: [
           {
-            name: `${rootLabel} spécifique 1.1`,
-            children: [
-              {
-                name: `${rootLabel} spécifique 1.1.1`,
-                children: [
-                  {
-                    name: `${rootLabel} spécifique 1.1.1.1`,
-                    route: `${routePrefix}/1/1-1/1-1-1/1-1-1-1`,
-                    exampleId,
-                  },
-                  {
-                    name: `${rootLabel} spécifique 1.1.1.2`,
-                    route: `${routePrefix}/1/1-1/1-1-1/1-1-1-2`,
-                  },
-                ],
-              },
-              {
-                name: `${rootLabel} spécifique 1.1.2`,
-                route: `${routePrefix}/1/1-1/1-1-2`,
-              },
-            ],
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
           },
           {
-            name: `${rootLabel} spécifique 1.2`,
-            route: `${routePrefix}/1/1-2`,
+            name: `Codification système`,
+            children: [
+              {
+                name: `Codification système`,
+                route: `parametrage/codification-systeme`
+              },
+              {
+                name: `Codification système alphanumérique`,
+                route: `parametrage/codification-systeme-alphanumerique`
+              }
+            ]
+          },
+          {
+            name: `Codification utilisateur alphanumérique`,
+            route: `parametrage/codification-utilisateur-alphanumerique`
+          },
+          {
+            name: `Codification super système`,
+            children: [
+              {
+                name: `Aide sur les données utilisateur`,
+                route: `parametrage/aide-donnees-utilisateur`
+              },
+              {
+                name: `Codification virtuelle numérique`,
+                route: `parametrage/codification-virtuelle-numerique`
+              },
+              {
+                name: `Codification virtuelle alphanumérique`,
+                route: `parametrage/codification-virtuelle-alphanumerique`
+              }
+            ]
+          },
+          {
+            name: `Pays, devises`,
+            children: [
+              {
+                name: `Pays`,
+                route: `parametrage/pays`
+              },
+              {
+                name: `Départements`,
+                route: `parametrage/departements`
+              },
+              {
+                name: `Devises`,
+                route: `parametrage/devises`
+              },
+              {
+                name: `Cours de change`,
+                route: `parametrage/cours-de-change`
+              }
+            ]
+          },
+          {
+            name: `Editions "Codification de base`,
+            children: [
+              {
+                name: `Codification Utilisateur`,
+                route: `parametrage/codification-utilisateur`
+              },
+              {
+                name: `Codification système`,
+                route: `parametrage/codification-systeme`
+              },
+              {
+                name: `Aide sur les données utilisateur`,
+                route: `parametrage/aide-donnees-utilisateur`
+              }
+            ]
           },
         ],
       },
       {
-        name: `${rootLabel} spécifique 2`,
-        route: `${routePrefix}/2`,
+        name: `Codification de soins de santé`,
+        children: [
+          {
+            name: `Paramétrage du parcours de soins`,
+            children: [
+              {
+                name: `Paramétrage des taux et plafonds`,
+                route: `parametrage/taux-plafonds`
+              },
+              {
+                name: `Spécialités médicales`,
+                route: `parametrage/specialite-medicales`
+              },
+              {
+                name: `Spécialités médicales exclues`,
+                route: `parametrage/specialite-medicales-exclues`
+              }
+            ],
+          },
+          {
+            name: `Régimes de base`,
+            route: `parametrage/regime-base`
+          },
+          {
+            name: `Familles/Actes`,
+            route: `parametrage/familles-actes`
+          },
+          {
+            name: `Liste des formules soins de santé`,
+            route: `parametrage/formules-soins-sante`
+          },
+          {
+            name: `Secteurs d'activité`,
+            route: `parametrage/secteurs-activite`
+          },
+          {
+            name: `Formules de prestations soins de santé`,
+            route: `parametrage/formules-prestations-soins-sante`
+          },
+          {
+            name: `Paramétrage des domaines Tiers payant`,
+            route: `parametrage/domaines-tiers-payant`
+          },
+          {
+            name: `Editions "Codification soins de santé"`,
+            children: [
+              {
+                name: `Codes actes`,
+                route: `parametrage/edition-codes-actes`
+              },
+              {
+                name: `Tarifs avec historique des valeurs`,
+                route: `parametrage/edition-tarifs-historique-valeurs`
+              },
+              {
+                name: `Formules de prestation soins de santé`,
+                route: `parametrage/edition-formules-prestation-soins-sante`
+              }
+            ],
+          }
+        ],
       },
+      {
+        name: `Paramétrage des portes externes`,
+        children: [
+          {
+            name: `Paramétrage des portes externes`,
+            route: `parametrage/portes-externes`
+          },
+          {
+            name: `Transcodification des actes`,
+            route: `parametrage/transcodification-actes`
+          },
+          {
+            name: `Zones de tarification`,
+            route: `parametrage/zones-tarification`
+          },
+          {
+            name: `Actes d'un régime`,
+            route: `parametrage/actes-regime`
+          },
+          {
+            name: `Paramétrage des I.E.M.I.E.`,
+            route: `parametrage/iemie`
+          },
+        ],
+      },
+      {
+        name: `Transcodifications externes`,
+        route: `parametrage/transcodifications-externes`
+      },
+      {
+        name: `Courriers, extractions`,
+        children: [
+          {
+            name: `Définition des modèles de courrier`,
+            route: `parametrage/definition-modeles-courrier`
+          },
+          {
+            name: `Définition des messages de courriers`,
+            route: `parametrage/definition-messages-courriers`
+          },
+          {
+            name: `Opérations de gestion "Courriers"`,
+            route: `parametrage/operations-gestion-courriers`
+          },
+          {
+            name: `Organisation des courriers`,
+            route: `parametrage/organisation-courriers`
+          },
+          {
+            name: `Edition des modèles de courriers généraux`,
+            route: `parametrage/edition-modeles-courrier-generaux`
+          }
+        ],
+      },
+      {
+        name: `Formules, indices, tableaux, ...`,
+        children: [
+          {
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
+          }
+        ],
+      },
+      {
+        name: `Utilisateurs, périphériques, habilitations`,
+        children: [
+          {
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
+          }
+        ],
+      },
+      {
+        name: `Comptabilité`,
+        children: [
+          {
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
+          }
+        ],
+      },
+      {
+        name: `Sociétés de gestion`,
+        children: [
+          {
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
+          }
+        ],
+      },
+      {
+        name: `Interfaces`,
+        children: [
+          {
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
+          }
+        ],
+      },
+      {
+        name: `Système`,
+        children: [
+          {
+            name: `Codification utilisateur`,
+            route: `parametrage/codification-utilisateur`
+          }
+        ],
+      }
     ],
-  };
-}
-
-export const NAV_TREE: NavTreeNode[] = [
-  buildBranch('Menu paramétrage', 'parametrage', 1),
-  buildBranch('Menu rapports', 'rapports', 2),
+  },
+  {
+    name: 'Personnes',
+    children: [
+      {
+        name: `X`,
+        children: [
+          {
+            name: `XXX`,
+            children: [
+              {
+                name: `XXXX`,
+                route: `parametrage/codification-systeme`
+              }
+            ]
+          },
+        ],
+      }
+    ],
+  },
+  {
+    name: 'Production',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Cotisations',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Prestations soins de santé',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Prestations prévoyance',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Opérations "Délégataires"',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Opérations "Délégants',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Opérations "Régimes Obligatoires"',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Trésorerie',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Fiscalité',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Comptabilisation',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Statistiques et pilotage',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Traitements différés',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  },
+  {
+    name: 'Interfaces',
+    children: [
+      {
+        name: `XXXX`,
+        route: `parametrage/codification-systeme`
+      }
+    ]
+  }
 ];
 
 export interface NavLeafRoute {
